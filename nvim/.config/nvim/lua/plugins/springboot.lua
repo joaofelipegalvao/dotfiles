6fab296982be200171a1ba92a55e0831e7326301
Joao Felipe Galvao <joaofelipe.galvao021@gmail.com>
2025-02-13 19:48:49 -0300

    Adds a delay to avoid invalid buffer error

--- nvim/.config/nvim/lua/plugins/springboot.lua
+++ nvim/.config/nvim/lua/plugins/springboot.lua
@@ -8,16 +8,18 @@ return {
       "mfussenegger/nvim-jdtls",
     },
     config = function()
-      local springboot_nvim = require("springboot-nvim")
+      vim.schedule(function()
+        local springboot_nvim = require("springboot-nvim")
 
-      -- Atalhos para interagir com Spring Boot
-      vim.keymap.set("n", "<leader>Jr", springboot_nvim.boot_run, { desc = "[J]ava [R]un Spring Boot" })
-      vim.keymap.set("n", "<leader>Jc", springboot_nvim.generate_class, { desc = "[J]ava Create [C]lass" })
-      vim.keymap.set("n", "<leader>Ji", springboot_nvim.generate_interface, { desc = "[J]ava Create [I]nterface" })
-      vim.keymap.set("n", "<leader>Je", springboot_nvim.generate_enum, { desc = "[J]ava Create [E]num" })
+        -- Atalhos para interagir com Spring Boot
+        vim.keymap.set("n", "<leader>Jr", springboot_nvim.boot_run, { desc = "[J]ava [R]un Spring Boot" })
+        vim.keymap.set("n", "<leader>Jc", springboot_nvim.generate_class, { desc = "[J]ava Create [C]lass" })
+        vim.keymap.set("n", "<leader>Ji", springboot_nvim.generate_interface, { desc = "[J]ava Create [I]nterface" })
+        vim.keymap.set("n", "<leader>Je", springboot_nvim.generate_enum, { desc = "[J]ava Create [E]num" })
 
-      -- Executa a configuração do plugin
-      springboot_nvim.setup({})
+        -- Executa a configuração do plugin
+        springboot_nvim.setup({})
+      end)
     end,
   },
 }

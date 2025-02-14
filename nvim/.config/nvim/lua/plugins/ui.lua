be57d0643404d699b792cf4b57b2482d3e6cc705
Joao Felipe Galvao <joaofelipe.galvao021@gmail.com>
2025-02-13 22:15:02 -0300

    disabling bufferline close icons

--- nvim/.config/nvim/lua/plugins/ui.lua
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -3,18 +3,10 @@ return {
   {
     "akinsho/bufferline.nvim",
     event = "VeryLazy",
-    keys = {
-      { "<Tab>", "<Cmd>BufferLineCycleNext<CR>", desc = "Next tab" },
-      { "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", desc = "Prev tab" },
-    },
-    opts = {
-      options = {
-        mode = "tabs",
-        -- separator_style = "slant",
-        show_buffer_close_icons = false,
-        show_close_icon = false,
-      },
-    },
+    opts = function(_, opts)
+      opts.options.show_buffer_close_icons = false
+      opts.options.show_close_icon = false
+    end,
   },
   {
     "b0o/incline.nvim",

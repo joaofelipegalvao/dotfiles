7cffcefcc29d42582c5261be599af186ca6d1750
Joao Felipe Galvao <joaofelipe.galvao021@gmail.com>
2025-02-13 22:10:39 -0300

    disabling default jdtls formatting

---
+++ nvim/.config/nvim/lua/plugins/java.lua
@@ -0,0 +1,12 @@
+return {
+  "mfussenegger/nvim-jdtls",
+  opts = function(_, opts)
+    opts.settings = vim.tbl_deep_extend("force", opts.settings or {}, {
+      java = {
+        format = {
+          enabled = false,
+        },
+      },
+    })
+  end,
+}

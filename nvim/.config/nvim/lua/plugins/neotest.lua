cb972b09d3f23e3975331e1c62459a67b3c5bda5
Joao Felipe Galvao <joaofelipe.galvao021@gmail.com>
2025-02-14 22:43:11 -0300

    Refactor neotest-jest setup to use a function with custom Jest options

--- nvim/.config/nvim/lua/plugins/neotest.lua
+++ nvim/.config/nvim/lua/plugins/neotest.lua
@@ -2,13 +2,22 @@ return {
   {
     "nvim-neotest/neotest",
     dependencies = {
-      "nvim-neotest/nvim-nio",
-      "haydenmeade/neotest-jest",
+      "haydenmeade/neotest-jest", -- Adaptador para Jest
     },
-    opts = {
-      adapters = {
-        "neotest-jest",
-      },
-    },
+    opts = function(_, opts)
+      -- Adiciona o adaptador neotest-jest à lista existente de adaptadores
+      table.insert(
+        opts.adapters,
+        require("neotest-jest")({
+          jestCommand = "npm test", -- Comando para executar os testes
+          jestConfigFile = "jest.config.js", -- Arquivo de configuração do Jest
+          env = { CI = true }, -- Variáveis de ambiente
+          cwd = function()
+            return vim.fn.getcwd() -- Diretório raiz do projeto
+          end,
+        })
+      )
+      return opts
+    end,
   },
 }

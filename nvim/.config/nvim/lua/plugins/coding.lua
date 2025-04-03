dfb1ce66239ab37e0ea7f0b79d10eeccf7729de5
Joao Felipe Galvao <joaofelipe.galvao021@gmail.com>
2025-04-03 10:25:51 -0300

    refactor(coding): remove symbols-outline and cmp-emoji plugins

--- nvim/.config/nvim/lua/plugins/coding.lua
+++ nvim/.config/nvim/lua/plugins/coding.lua
@@ -14,21 +14,4 @@ return {
       })
     end,
   },
-
-  {
-    "simrat39/symbols-outline.nvim",
-    keys = { { "<leader>cs", "<cmd>SymbolsOutline<cr>", desc = "Symbols Outline" } },
-    cmd = "SymbolsOutline",
-    opts = {
-      position = "right",
-    },
-  },
-
-  {
-    "nvim-cmp",
-    dependencies = { "hrsh7th/cmp-emoji" },
-    opts = function(_, opts)
-      table.insert(opts.sources, { name = "emoji" })
-    end,
-  },
 }
